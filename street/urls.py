bc17fabaa5494ec8ee1d06d7305905750affce97
Blankphrase <ckasera6486@gmail.com>
2018-08-07 12:20:57 +0300

    creation of hoods urls

---
+++ street/urls.py
@@ -0,0 +1,27 @@
+from django.conf.urls import url
+from django.conf import settings
+from . import views
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    url('^$', views.index, name='index'),
+    url('^home/', views.home, name='home'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
+    url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
+    url(r'^accounts/edit/', views.edit_profile, name='editprofile'),
+    url(r'^comment/(?P<post_id>\d+)', views.comment, name='comment'),
+    url(r'^hood/$', views.hood, name='hood'),
+    url(r'^join/(\d+)', views.join, name='join'),
+    url(r'^exitHood/(\d+)', views.exitHood, name='exitHood'),
+    url(r'^myHood/$', views.hoodHome, name='myHood'),
+    url(r'^business/$', views.business, name='business'),
+    url(r'^allBusinesses/$', views.allbusinesses, name='allBusinesses'),
+    url(r'^searchBusiness/$', views.search, name='search'),
+    url(r'^createPost/$', views.createPost, name='createPost'),
+    url(r'^allPosts/$',views.allPosts, name='allPosts'),
+    url(r'^myPosts/$', views.myPosts, name='myPosts'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
