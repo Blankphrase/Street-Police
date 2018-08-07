0cdb7b3913e8f3eb4e053a7dd6bed5924db48f10
Blankphrase <ckasera6486@gmail.com>
2018-08-07 11:22:15 +0300

    creation of profile form

--- street/forms.py
+++ street/forms.py
@@ -1,9 +1,17 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Neighbourhood, Business, Post, Profile, Comments
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields=['bio', 'avatar']
+
+class PostForm
